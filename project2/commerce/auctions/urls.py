9b3d5e361f108fcff9b669418b19e23d74685cdc
alekya-damagari <108224766+alekya-damagari@users.noreply.github.com>
2023-05-19 13:49:59 +0530

    Add files via upload
    Commerce : An e-commerce auction site

---
+++ project2/commerce/auctions/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("create", views.createListing, name="create"),
+    
+    path("displayCategory", views.displayCategory, name="displayCategory"),
+    path("listing/<int:id>", views.listing, name="listing"),
+    #path("watchlist", views.displayWatchlist, name="watchlist"),
+    path("addWatchlist/<int:id>", views.addWatchlist, name="addWatchlist"),
+    path("removeWatchlist/<int:id>", views.removeWatchlist, name="removeWatchlist"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("addComment/<int:id>", views.addComment, name="addComment"),
+    path("addBid/<int:id>", views.addBid, name="addBid"),
+    path("closeAuction/<int:id>", views.closeAuction, name="closeAuction"),
+
+
+    path("categories", views.categories, name="categories"),
+    path("categories/<int:category_id>", views.categories_choose, name="categories_choose"),
+
+
+]
